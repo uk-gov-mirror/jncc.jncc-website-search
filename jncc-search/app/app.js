1c7f09f8ad2b5c760d128cf8039b4943b51200ae
CathyJinJNCC <cathy.jin@jncc.gov.uk>
2020-05-22 16:52:59 +0100

    Hooking up rest of query parameters

--- jncc-search/app/app.js
+++ jncc-search/app/app.js
@@ -12,6 +12,13 @@ const sortOptions = {
     DATE_DESC: 'date_desc'
 }
 
+const views = {
+    PAGES: 'pages',
+    RESOURCES: 'resources'
+}
+
+const pageSize = 10
+
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
@@ -32,42 +39,69 @@ exports.lambdaHandler = async (event, context) => {
     var htmlBody = ''
 
     // default values
+    var view = views.PAGES
     var queryTerms = ''
     var sortOption = sortOptions.RELEVANCE
     var pageStart = 0
-    var pageSize = 10
+    var filters = []
 
     try {
-        console.log('Starting jncc-search lambda');
+        console.log('Starting jncc-search lambda')
 
         if (event.queryStringParameters) {
-            console.log("Received query string parameters: " + JSON.stringify(event.queryStringParameters));
+            console.log("Received query string parameters")
+            if (event.queryStringParameters.v) {
+                view = event.queryStringParameters.v
+                console.log(`View: ${view}`)
+            }
             if (event.queryStringParameters.q) {
-                queryTerms = event.queryStringParameters.q;
+                queryTerms = event.queryStringParameters.q
+                console.log(`Query terms: ${queryTerms}`)
             }
             if (event.queryStringParameters.s) {
-                sortOption = event.queryStringParameters.s;
+                sortOption = event.queryStringParameters.s
+                console.log(`Sort option: ${sortOption}`)
             }
-            if (event.queryStringParameters.q) {
-                pageStart = event.queryStringParameters.p * pageSize;
+            if (event.queryStringParameters.p) {
+                pageStart = event.queryStringParameters.p * pageSize
+                console.log(`Page start: ${pageStart}`)
             }
         }
 
-        var payload = buildEsPageQuery(queryTerms, sortOption, pageStart, pageSize)
-        response = await queryElasticsearch(payload)
+        if (event.multiValueQueryStringParameters && event.multiValueQueryStringParameters.f) {
+            console.log("Received multi value parameters")
+            filters = event.multiValueQueryStringParameters.f
+            console.log("Filters: " + JSON.stringify(filters))
+        }
 
-        var pages = JSON.parse(response.body).hits.hits
+        var payload = null
+        if (view == views.RESOURCES) {
+            payload = await buildEsResourceQuery(queryTerms, filters, sortOption, pageStart, pageSize)
+        } else {
+            payload = await buildEsPageQuery(queryTerms, sortOption, pageStart, pageSize)
+        }
+        
+        var hits = null
+        await queryElasticsearch(payload).then(
+            response => {
+                hits = JSON.parse(response.body).hits
+                console.log(`Successfully got response with ${hits.total} results`)
+            },
+            err => {
+                console.error(`Resource search query failed with error ${err}`)
+            })
 
-        ejs.renderFile('index.ejs', {pages: pages}, (err, html) => {
+        // populate the template
+        ejs.renderFile('index.ejs', {view: view, hits: hits}, (err, html) => {
             if (err) {
-                console.log(err)
-                return err
+                console.error(`HTML template rendering failed with error ${err}`)
+                throw new Error()
             }
             htmlBody = html
         });
     } catch (err) {
-        console.log(err)
-        return err
+        console.error(`Could not generate search results page, got error ${err}`)
+        htmlBody = `Oops something went wrong`
     }
 
     response.headers = {
@@ -104,23 +138,29 @@ function buildEsPageQuery(queryTerms, sortOption, pageStart, pageSize) {
             }
         }
     }
-    query = addSortOrderToQuery(sortOption, query)
+    addSortOrderToQuery(sortOption, query)
 
     return query
 }
 
-function buildEsResourceQuery(queryTerms, sortOption, pageStart, pageSize) {
+function buildEsResourceQuery(queryTerms, filters, sortOption, pageStart, pageSize) {
     var query = {
         "query": {
             "bool": {
-                "should": [
-                    { "term": { "title": queryTerms } },
-                    { "term": { "content": queryTerms } }
+                "must": [
+                    {
+                        "bool": {
+                            "should": [
+                                { "term": { "title": queryTerms } },
+                                { "term": { "content": queryTerms } }
+                            ],
+                            "minimum_should_match" : 1
+                        }
+                    }
                 ],
                 "filter": [
                     { "exists": { "field": "resource_type" } }
-                ],
-                "minimum_should_match" : 1
+                ]
             }
         },
         "from": pageStart,
@@ -142,15 +182,49 @@ function buildEsResourceQuery(queryTerms, sortOption, pageStart, pageSize) {
             }
         }
     }
-    query = addSortOrderToQuery(sortOption, query)
+    addFiltersToQuery(filters, query)
+    addSortOrderToQuery(sortOption, query)
 
     return query
+}
+
+function addFiltersToQuery(filters, query) {
+    var queryFilters = []
+    filters.forEach(filter => {
+        if (filter === 'other') {
+            // assets with no files
+            queryFilters.push({
+                "bool": {
+                    "must_not": {
+                        "exists": {
+                            "field": "file_extension"
+                        }
+                    }
+                }
+            })
+        } else {
+            queryFilters.push({
+                "term": {
+                    "file_extension": filter
+                }
+            })
+        }
+    })
+
+    if (filters.length > 0) {
+        query.query.bool.must.push({
+            "bool": {
+                "should": queryFilters,
+                "minimum_should_match" : 1
+            }
+        })
+    }
 }
 
 function addSortOrderToQuery(sortOption, query) {
     switch (sortOption) {
         case sortOptions.RELEVANCE:
-            query.querysort = '_source'
+            query.sort = '_score'
             break
         case sortOptions.TITLE_ASC:
             query.sort = [ { "title.raw": "asc" } ]
@@ -164,9 +238,9 @@ function addSortOrderToQuery(sortOption, query) {
         case sortOptions.DATE_DESC:
             query.sort = [ { "published_date": "desc" } ]
             break
+        default:
+            console.warn(`Sort option ${sortOption} was not recognised, query not changed`)
     }
-    console.warn(`Sort option ${sortOption} was not recognised, query not changed`)
-    return query
 }
 
 async function queryElasticsearch(payload) {
@@ -186,7 +260,13 @@ async function queryElasticsearch(payload) {
     
         var client = new AWS.HttpClient();
         client.handleRequest(req, null, function(httpResp) {
-            console.log(httpResp.statusCode + ' ' + httpResp.statusMessage);
+            console.log(`Got response from elasticsearch ${httpResp.statusCode} ${httpResp.statusMessage}`);
+            if (httpResp.statusCode != 200) {
+                reject({
+                    statusCode: httpResp.statusCode,
+                    body: httpResp.statusMessage
+                })
+            }
             var respBody = '';
             httpResp.on('data', function (chunk) {
                 respBody += chunk;
@@ -202,7 +282,7 @@ async function queryElasticsearch(payload) {
             reject({
                 statusCode: 500,
                 body: 'Something went wrong!'
-            });
-        });
-    });
+            })
+        })
+    })
 }
